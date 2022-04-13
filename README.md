4d17f98d54cf4037e9a6bc48223fa59896f19aaf
0x09F911029D74E35BD84156C5635688C0 <acidicbacteria@riseup.net>
2022-04-13 13:17:09 +0000

    build scripts, patches, nginx-quic + quictls config

--- README.md
+++ README.md
@@ -1,20 +1,71 @@
 # Plex Nginx Reverse Proxy
  
-This configuration will allow you to serve Plex via Nginx.
+This configuration will allow you to serve Plex via Nginx behind CloudFlare
+
+ * Originally based on https://github.com/toomuchio/plex-nginx-reverseproxy
+ * Build script based on https://github.com/MatthewVance/nginx-build
+
+## Features
+
+```
+nginx version: nginx/1.21.7 (04132022-131108-UTC-[debian_nginx-quic_quictls])
+built by gcc 11.2.0 (GCC)
+built with OpenSSL 3.0.2+quic 15 Mar 2022
+TLS SNI support enabled
+configure arguments: --build=04132022-131108-UTC-[debian_nginx-quic_quictls] --prefix=/etc/nginx --with-cc-opt='-I/usr/local/include -m64 -march=native -DTCP_FASTOPEN=23 -falign-functions=32 -g -O3 -Wno-error=strict-aliasing -Wno-vla-parameter -fstack-protector-strong -flto=8 -fuse-ld=gold --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wno-error=pointer-sign -Wimplicit-fallthrough=0 -fcode-hoisting -Wp,-D_FORTIFY_SOURCE=2 -Wno-deprecated-declarations' --with-ld-opt='-Wl,-E -L/usr/local/lib -ljemalloc -Wl,-z,relro -Wl,-rpath,/usr/local/lib -flto=8 -fuse-ld=gold' --with-openssl=../openssl --with-openssl-opt='no-weak-ssl-ciphers no-ssl2 no-ssl3 no-idea no-err no-srp no-psk no-nextprotoneg enable-ktls enable-zlib enable-ec_nistp_64_gcc_128' --with-zlib=../zlib --with-pcre=../pcre-8.45 --sbin-path=/usr/sbin/nginx --modules-path=/usr/lib/nginx/modules --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --http-client-body-temp-path=/var/cache/nginx/client_temp --http-proxy-temp-path=/var/cache/nginx/proxy_temp --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp --http-scgi-temp-path=/var/cache/nginx/scgi_temp --user=nginx --group=nginx --with-file-aio --with-threads --with-libatomic --with-http_ssl_module --with-http_v2_module --with-http_v3_module --with-http_realip_module --add-module=../ngx_brotli --add-module=../headers-more-nginx-module-0.33 --without-http_charset_module --without-http_ssi_module --without-http_auth_basic_module --without-http_mirror_module --without-http_autoindex_module --without-http_userid_module --without-http_geo_module --without-http_split_clients_module --without-http_referer_module --without-http_fastcgi_module --without-http_uwsgi_module --without-http_scgi_module --without-http_grpc_module --without-http_memcached_module --without-http_limit_conn_module --without-http_limit_req_module --without-http_empty_gif_module --without-http_browser_module --without-http_upstream_hash_module --without-http_upstream_ip_hash_module --without-http_upstream_least_conn_module --without-http_upstream_zone_module --without-mail_imap_module --without-mail_pop3_module --without-mail_smtp_module
+```
+
+ * nginx-quic 1.21.7 - https://hg.nginx.org/nginx-quic
+ * openssl-3.0.2+quic - quictls: https://github.com/quictls/openssl/tree/openssl-3.0.2+quic
+ * Kernel TLS - https://www.nginx.com/blog/improving-nginx-performance-with-kernel-tls
+ * QUIC transport protocol and HTTP/3 - https://www.nginx.com/blog/introducing-technology-preview-nginx-support-for-quic-http-3/
+ * Add HTTP2 HPACK Encoding Support - https://blog.cloudflare.com/hpack-the-silent-killer-feature-of-http-2/
+ * Add Dynamic TLS Record support - https://blog.cloudflare.com/optimizing-tls-over-tcp-to-reduce-latency/
+ * Hide Server Signature - https://github.com/torden/ngx_hidden_signature_patch 
+ * brotli compression support - https://github.com/google/ngx_brotli
+ * More Headers - https://github.com/openresty/headers-more-nginx-module
+ * Cloudflare ZLIB - https://github.com/cloudflare/zlib
+ * Prevents public access to PMS built-in web interface
+ * tmpfs cache (1GB) for Plex images
  
-## Minimal Requirements
- 
-Nginx
+## Requirements
  
 Plex:
 * Remote Access - Disable
 * Network - Custom server access URLs = `https://<your-domain>:443,http://<your-domain>:80`
-* Network - Secure connections = Preferred.
-* ![#f03c15](https://placehold.it/15/f03c15/000000?text=+) `Note you can force SSL by setting required and not adding the HTTP URL, however some players which do not support HTTPS (e.g: Roku, Playstations, some SmartTVs) will no longer function.`
- 
-## Optional Requirements
- 
-UFW or other firewall:
-* Deny port 32400 externally (Plex still pings over 32400, some clients may use 32400 by mistake despite 443 and 80 being set).
-* Note adding `allowLocalhostOnly="1"` to your Preferences.xml, will make Plex only listen on the localhost, achieving the same thing as using a firewall.
+* Network - Secure connections = Preferred
 
+System: 
+* Debian Bullseye x64 (11)
+* Custom kernel with Kernel TLS module (CONFIG_TLS=y) - https://www.nginx.com/blog/improving-nginx-performance-with-kernel-tls
+* tmpfs for cache - add "tmpfs /var/cache/nginx/ram_cache/ tmpfs defaults,size=1024M 0 0" to fstab
+
+* gcc 11.2.0 
+
+```
+Using built-in specs.
+COLLECT_GCC=gcc
+COLLECT_LTO_WRAPPER=/usr/local/gcc-11.2.0/libexec/gcc/x86_64-linux-gnu/11.2.0/lto-wrapper
+Target: x86_64-linux-gnu
+Configured with: ../gcc-11.2.0/configure -v --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --prefix=/usr/local/gcc-11.2.0 --with-system-zlib --enable-checking=release --enable-languages=c,c++,d,fortran,go,objc,obj-c++ --disable-multilib --program-suffix=-11.2
+Thread model: posix
+Supported LTO compression algorithms: zlib
+gcc version 11.2.0 (GCC)
+```
+
+Cloudflare:
+* SSL: https://support.cloudflare.com/hc/en-us/categories/200276247-SSL-TLS
+* Disable ipv6 connectivity
+
+iptables:
+* Deny port 32400 externally (Plex still pings over 32400, some clients may use 32400 by mistake despite 443 and 80 being set)
+* Note adding `allowLocalhostOnly="1"` to your Preferences.xml, will make Plex only listen on the localhost, achieving the same thing as using a firewall
+* Only allow CloudFlare IPs via iptables using ipset
+
+```
+ipset create cf hash:net
+for x in $(curl https://www.cloudflare.com/ips-v4); do ipset add cf $x; done
+iptables -A INPUT -p tcp -m tcp --dport 32400 -j DROP
+iptables -A INPUT -m set --match-set cf src -p tcp -m multiport --dports http,https -j ACCEPT
+iptables -A INPUT -m set --match-set cf src -p udp -m multiport --dports https -j ACCEPT
+```
---
+++ build-nginx.sh
@@ -0,0 +1,242 @@
+#!/usr/bin/env bash
+# Run as root or with sudo
+
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root or with sudo."
+  exit 1
+fi
+
+set -e
+
+## Set names of latest versions of each package
+VERSION_PCRE=pcre-8.45
+VERSION_NGINX=1.21.7
+VERSION_LIBATOMIC=7.6.12
+VERSION_HEADERS=0.33
+
+## Set URLs to the source directories
+SOURCE_PCRE=https://ftp.exim.org/pub/pcre/
+SOURCE_ZLIB=https://github.com/cloudflare/zlib
+SOURCE_NGINX=https://hg.nginx.org/nginx-quic
+SOURCE_LIBATOMIC=https://github.com/ivmai/libatomic_ops/archive/v
+SOURCE_BROTLI=https://github.com/google/ngx_brotli
+SOURCE_HEADERS=https://github.com/openresty/headers-more-nginx-module/archive/v
+SOURCE_JEMALLOC=https://github.com/jemalloc/jemalloc
+SOURCE_OPENSSL=https://github.com/quictls/openssl
+
+## Set where OpenSSL and NGINX will be built
+SPATH=$(pwd)
+BPATH=$SPATH/build
+TIME=$(date +%m%d%Y-%H%M%S-%Z)
+
+## Clean screen before launching
+clear
+
+## Clean out any files from previous runs of this script
+rm -rf \
+  "$BPATH"
+mkdir "$BPATH"
+rm -rf \
+  "$SPATH/packages"
+mkdir "$SPATH/packages"
+
+## Move tmp within build directory
+mkdir "$BPATH/tmp"
+export TMPDIR="$BPATH/tmp"
+
+## Ensure the required software to compile NGINX is installed
+apt-get update && apt-get -y install \
+  binutils \
+  build-essential \
+  checkinstall \
+  cmake \
+  autoconf \
+  automake \
+  libtool \
+  mercurial \
+  git \
+  curl \
+  wget
+
+## Download the source files
+curl -L "${SOURCE_PCRE}${VERSION_PCRE}.tar.gz" -o "${BPATH}/pcre.tar.gz"
+curl -L "${SOURCE_LIBATOMIC}${VERSION_LIBATOMIC}.tar.gz" -o "${BPATH}/libatomic.tar.gz"
+curl -L "${SOURCE_HEADERS}${VERSION_HEADERS}.tar.gz" -o "${BPATH}/headers.tar.gz"
+cd "$BPATH"
+git clone $SOURCE_ZLIB --branch gcc.amd64
+git clone $SOURCE_JEMALLOC
+git clone --depth=1 --recurse-submodules $SOURCE_BROTLI
+git clone --recursive $SOURCE_OPENSSL
+hg clone -b quic $SOURCE_NGINX
+
+## Expand the source files
+cd "$BPATH"
+for ARCHIVE in ./*.tar.gz; do
+  tar xzf "$ARCHIVE"
+done
+
+## Clean up source files
+rm -rf \
+  "$BPATH"/*.tar.*
+
+## Create NGINX cache directories if they do not already exist
+if [ ! -d "/var/cache/nginx/" ]; then
+  mkdir -p \
+    /var/cache/nginx/client_temp \
+    /var/cache/nginx/proxy_temp \
+    /var/cache/nginx/fastcgi_temp \
+    /var/cache/nginx/uwsgi_temp \
+    /var/cache/nginx/scgi_temp \
+    /var/cache/nginx/ram_cache
+fi
+
+## We add sites-* folders as some use them. /etc/nginx/conf.d/ is the vhost folder by defaultnginx
+if [[ ! -d /etc/nginx/sites-available ]]; then
+	mkdir -p /etc/nginx/sites-available
+	cp "$SPATH/conf/plex.domain.tld" "/etc/nginx/sites-available/plex.domain.tld"
+fi
+if [[ ! -d /etc/nginx/sites-enabled ]]; then
+	mkdir -p /etc/nginx/sites-enabled
+fi
+
+if [[ ! -e /etc/nginx/nginx.conf ]]; then
+	mkdir -p /etc/nginx
+	cd /etc/nginx || exit 1
+	cp "$SPATH/conf/nginx.conf" "/etc/nginx/nginx.conf"
+fi
+
+## Add NGINX group and user if they do not already exist
+id -g nginx &>/dev/null || addgroup --system nginx
+id -u nginx &>/dev/null || adduser --disabled-password --system --home /var/cache/nginx --shell /sbin/nologin --group nginx
+
+## make libatomic
+cd "$BPATH/libatomic_ops-$VERSION_LIBATOMIC"
+autoreconf -i
+./configure
+make -j "$(nproc)"
+make install
+
+## make zlib
+cd "$BPATH/zlib"
+./configure
+make -j "$(nproc)"
+
+## make jemalloc
+cd "$BPATH/jemalloc"
+./autogen.sh
+make -j "$(nproc)"
+checkinstall --install=no --pkgversion=$(cat "$BPATH/jemalloc/VERSION") -y
+cp "$BPATH/jemalloc"/*.deb "$SPATH/packages"
+make install
+
+ldconfig
+
+## Build NGINX, with various modules included/excluded; requires GCC 11.2
+cd "$BPATH/ngx_brotli"
+readlink -f config
+
+cd "$BPATH/nginx-quic"
+patch -p1  < "$SPATH/patches/nginx.patch"
+patch -p1  < "$SPATH/patches/nginx-1.14.x-1.17.x-ngx_http_header_filter_module.c.patch"
+patch -p1  < "$SPATH/patches/nginx-1.14.x-1.17.x-ngx_http_special_response.c.patch"
+
+./auto/configure \
+  --build="$TIME-[debian_nginx-quic_quictls]" \
+  --prefix=/etc/nginx \
+  --with-cc-opt='-I/usr/local/include -m64 -march=native -DTCP_FASTOPEN=23 -falign-functions=32 -g -O3 -Wno-error=strict-aliasing -Wno-vla-parameter -fstack-protector-strong -flto=8 -fuse-ld=gold --param=ssp-buffer-size=4 -Wformat -Werror=format-security -Wno-error=pointer-sign -Wimplicit-fallthrough=0 -fcode-hoisting -Wp,-D_FORTIFY_SOURCE=2 -Wno-deprecated-declarations' \
+  --with-ld-opt='-Wl,-E -L/usr/local/lib -ljemalloc -Wl,-z,relro -Wl,-rpath,/usr/local/lib -flto=8 -fuse-ld=gold' \
+  --with-openssl="../openssl" \
+  --with-openssl-opt='no-weak-ssl-ciphers no-ssl2 no-ssl3 no-idea no-err no-srp no-psk no-nextprotoneg enable-ktls enable-zlib enable-ec_nistp_64_gcc_128' \
+  --with-zlib="../zlib" \
+  --with-pcre="../pcre-8.45" \
+  --sbin-path=/usr/sbin/nginx \
+  --modules-path=/usr/lib/nginx/modules \
+  --conf-path=/etc/nginx/nginx.conf \
+  --error-log-path=/var/log/nginx/error.log \
+  --http-log-path=/var/log/nginx/access.log \
+  --pid-path=/var/run/nginx.pid \
+  --lock-path=/var/run/nginx.lock \
+  --http-client-body-temp-path=/var/cache/nginx/client_temp \
+  --http-proxy-temp-path=/var/cache/nginx/proxy_temp \
+  --http-fastcgi-temp-path=/var/cache/nginx/fastcgi_temp \
+  --http-uwsgi-temp-path=/var/cache/nginx/uwsgi_temp \
+  --http-scgi-temp-path=/var/cache/nginx/scgi_temp \
+  --user=nginx \
+  --group=nginx \
+  --with-file-aio \
+  --with-threads \
+  --with-libatomic \
+  --with-http_ssl_module \
+  --with-http_v2_module \
+  --with-http_v3_module \
+  --with-http_realip_module \
+  --add-module="../ngx_brotli" \
+  --add-module="../headers-more-nginx-module-0.33" \
+  --without-http_charset_module \
+  --without-http_ssi_module \
+  --without-http_auth_basic_module \
+  --without-http_mirror_module \
+  --without-http_autoindex_module \
+  --without-http_userid_module \
+  --without-http_geo_module \
+  --without-http_split_clients_module \
+  --without-http_referer_module \
+  --without-http_fastcgi_module \
+  --without-http_uwsgi_module \
+  --without-http_scgi_module \
+  --without-http_grpc_module \
+  --without-http_memcached_module \
+  --without-http_limit_conn_module \
+  --without-http_limit_req_module \
+  --without-http_empty_gif_module \
+  --without-http_browser_module \
+  --without-http_upstream_hash_module \
+  --without-http_upstream_ip_hash_module \
+  --without-http_upstream_least_conn_module \
+  --without-http_upstream_zone_module \
+  --without-mail_imap_module \
+  --without-mail_pop3_module \
+  --without-mail_smtp_module
+
+make -j "$(nproc)"
+make install
+checkinstall --install=no --pkgname="nginx-quic" --pkgversion="$VERSION_NGINX" -y
+cp "$BPATH/nginx-quic"/*.deb "$SPATH/packages"
+make clean
+strip -s /usr/sbin/nginx*
+
+## Create NGINX systemd service file if it does not already exist
+if [ ! -e "/lib/systemd/system/nginx.service" ]; then
+  # Control will enter here if the NGINX service doesn't exist.
+  file="/lib/systemd/system/nginx.service"
+
+  /bin/cat >$file <<'EOF'
+[Unit]
+Description=A high performance web server and a reverse proxy server
+After=network.target nss-lookup.target
+
+[Service]
+Type=forking
+PIDFile=/run/nginx.pid
+ExecStartPre=/usr/sbin/nginx -t -q -g 'daemon on; master_process on;'
+ExecStart=/usr/sbin/nginx -g 'daemon on; master_process on;'
+ExecStartPost=/bin/sleep 0.1
+ExecReload=/usr/sbin/nginx -g 'daemon on; master_process on;' -s reload
+ExecStop=-/sbin/start-stop-daemon --quiet --stop --retry QUIT/5 --pidfile /run/nginx.pid
+TimeoutStopSec=5
+KillMode=mixed
+
+[Install]
+WantedBy=multi-user.target
+EOF
+fi
+
+clear 
+
+echo "SUCCESS"
+
+nginx -V
+
+## Clean out any files from this script
+rm -rf \
+  "$BPATH"
